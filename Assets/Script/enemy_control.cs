c4d135c588ce137a7cfa573b70fefa40a2837b14
micktsai <mick14758@gmail.com>
2021-12-23 22:50:28 +0800

    enemy bug fix v1

--- Assets/Script/enemy_control.cs
+++ Assets/Script/enemy_control.cs
@@ -13,7 +13,7 @@ public class enemy_control : MonoBehaviour
     float direction;
     public bool hit;
     public Transform player;
-    float Distance = 20.0f;
+    float Distance = 15.0f;
     float timer;
     // Start is called before the first frame update
     void Start()
@@ -53,7 +53,7 @@ public class enemy_control : MonoBehaviour
         if(Vector3.Distance(player.position,this.transform.position) < Distance)
         {
             this.transform.LookAt(player.position);
-            rg.velocity = (this.transform.forward + new Vector3(0, 0, 0)) * speed * Time.deltaTime;
+            rg.velocity = (this.transform.forward ) * speed * Time.deltaTime;
             return;
         }
         if (step == rand)
@@ -63,7 +63,7 @@ public class enemy_control : MonoBehaviour
             step = 0;
         }
         transform.rotation = Quaternion.Euler(0, direction, 0);
-        rg.velocity = (this.transform.forward + new Vector3(0, -10, 0)) * speed * Time.deltaTime;
+        rg.velocity = (this.transform.forward ) * speed * Time.deltaTime;
         Debug.Log(rg.velocity);
     }
 }
